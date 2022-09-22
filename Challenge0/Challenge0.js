293c5e4d4ce46e0a8a176541eee4eb3f5e9246e8
HELPER593 <jesiel_1990_@hotmail.com>
2022-09-22 04:59:04 -0500

    reto0

---
+++ Challenge0/Challenge0.js
@@ -0,0 +1,38 @@
+
+/*
+ * Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Fecha publicación enunciado: 27/12/21
+ * Fecha publicación resolución: 03/01/22
+ * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+function fizzbuzz() {
+    var mult3, mult5;
+  
+    for (var i = 1, _pj_a = 101; i < _pj_a; i += 1) {
+      mult3 = i % 3 === 0;
+      mult5 = i % 5 === 0;
+  
+      if (mult3 && mult5) {
+        console.log("fizzbuzz");
+      } else {
+        if (mult3) {
+          console.log("fizz");
+        } else {
+          if (mult5) {
+            console.log("buzz");
+          } else {
+            console.log(i);
+          }
+        }
+      }
+    }
+  }
+  
+  fizzbuzz();
+  
