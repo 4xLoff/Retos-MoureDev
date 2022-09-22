c61daf5e3445c65b8c875ee1796706841b90c13e
HELPER593 <jesiel_1990_@hotmail.com>
2022-09-22 04:59:15 -0500

    ret0

---
+++ Challenge0/Challenge0.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/bash
+
+<< 'Comment'
+ * Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Fecha publicación enunciado: 27/12/21
+ * Fecha publicación resolución: 03/01/22
+ * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+Comment
+
+function fizz_buzz {
+  
+  for number in $(seq 1 100); do
+      if [[ $((number%3 )) == 0 && $((number%5)) == 0 ]]; then
+          echo "fizzbuzz"
+      elif [[ $((number%3)) -eq 0 ]]; then
+          echo "fizz"
+      elif [[ $((number%5)) == 0 ]]; then
+          echo  "buzz"
+      else
+          echo "$number"
+      fi
+  done
+}
+
+fizz_buzz
